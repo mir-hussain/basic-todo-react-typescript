fa44adf80cc9443a9a0d437bdec36fb3130e6c05
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-13 12:20:54 +0600

    basic input added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <div className='bg-gray-50 h-screen flex items-center justify-center  '>
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
